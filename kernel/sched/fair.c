98682465ef44cc88d41e4b6238480dbb74fbc2db
Pranav Vashi <neobuddy89@gmail.com>
2013-12-14 21:16:53 -0800

    sched: Add SysInterface for GENTLE_FAIR_SLEEPERS
    Change-Id: Iee8de1732abf464876503599298275983b1ea6e9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4762,6 +4762,12 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
+static unsigned int Lgentle_fair_sleepers = 1;
+void relay_gfs(unsigned int gfs)
+{
+	Lgentle_fair_sleepers = gfs;
+}
+
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
@@ -4784,8 +4790,8 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		 * Halve their sleep time's effect, to allow
 		 * for a gentler effect of sleepers:
 		 */
-		if (sched_feat(GENTLE_FAIR_SLEEPERS))
-			thresh >>= 0;
+		if (Lgentle_fair_sleepers)
+			thresh >>= 1;
 
 		vruntime -= thresh;
 	}
