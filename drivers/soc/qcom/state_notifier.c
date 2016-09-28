89a7a0f5438cdcbc5f4d67757e8c997e7b30351f
Pranav Vashi <neobuddy89@gmail.com>
2016-09-28 20:50:15 +0530

    Add State Notifier Driver
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/soc/qcom/state_notifier.c
+++ drivers/soc/qcom/state_notifier.c
@@ -1,7 +1,7 @@
 /*
  * State Notifier Driver
  *
- * Copyright (c) 2013-2015, Pranav Vashi <neobuddy89@gmail.com>
+ * Copyright (c) 2013-2016, Pranav Vashi <neobuddy89@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -9,14 +9,11 @@
  *
  */
 
-#include <linux/state_notifier.h>
-#include <linux/notifier.h>
 #include <linux/export.h>
-#include <linux/fb.h>
 #include <linux/module.h>
+#include <linux/state_notifier.h>
 
 #define DEFAULT_SUSPEND_DEFER_TIME 	10
-#define DEFAULT_USE_FB_NOTIFIER 	0
 #define STATE_NOTIFIER			"state_notifier"
 
 /*
@@ -31,8 +28,8 @@ do {				\
 		pr_info(msg);	\
 } while (0)
 
-static struct notifier_block notif;
-static int prev_fb = FB_BLANK_UNBLANK;
+static bool enabled;
+module_param_named(enabled, enabled, bool, 0664);
 static unsigned int suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME;
 module_param_named(suspend_defer_time, suspend_defer_time, uint, 0664);
 static struct delayed_work suspend_work;
@@ -41,8 +38,6 @@ struct work_struct resume_work;
 bool state_suspended;
 module_param_named(state_suspended, state_suspended, bool, 0444);
 static bool suspend_in_progress;
-bool use_fb_notifier = DEFAULT_USE_FB_NOTIFIER;
-module_param_named(use_fb_notifier, use_fb_notifier, bool, 0664);
 
 static BLOCKING_NOTIFIER_HEAD(state_notifier_list);
 
@@ -80,28 +75,27 @@ EXPORT_SYMBOL_GPL(state_notifier_call_chain);
 
 static void _suspend_work(struct work_struct *work)
 {
-	state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
 	state_suspended = true;
+	state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
 	suspend_in_progress = false;
 	dprintk("%s: suspend completed.\n", STATE_NOTIFIER);
 }
 
 static void _resume_work(struct work_struct *work)
 {
-	state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
 	state_suspended = false;
+	state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
 	dprintk("%s: resume completed.\n", STATE_NOTIFIER);
 }
 
 void state_suspend(void)
 {
 	dprintk("%s: suspend called.\n", STATE_NOTIFIER);
-	if (state_suspended || suspend_in_progress)
+	if (state_suspended || suspend_in_progress || !enabled)
 		return;
 
 	suspend_in_progress = true;
 
-	INIT_DELAYED_WORK(&suspend_work, _suspend_work);
 	queue_delayed_work_on(0, susp_wq, &suspend_work, 
 		msecs_to_jiffies(suspend_defer_time * 1000));
 }
@@ -109,7 +103,6 @@ void state_suspend(void)
 void state_resume(void)
 {
 	dprintk("%s: resume called.\n", STATE_NOTIFIER);
-	flush_workqueue(susp_wq);
 	cancel_delayed_work_sync(&suspend_work);
 	suspend_in_progress = false;
 
@@ -117,57 +110,19 @@ void state_resume(void)
 		queue_work_on(0, susp_wq, &resume_work);
 }
 
-static int fb_notifier_callback(struct notifier_block *self,
-				unsigned long event, void *data)
-{
-	struct fb_event *evdata = data;
-	int *blank;
-
-	if (!use_fb_notifier)
-		return NOTIFY_OK;
-
-	if (evdata && evdata->data && event == FB_EVENT_BLANK) {
-		blank = evdata->data;
-		switch (*blank) {
-			case FB_BLANK_UNBLANK:
-				if (prev_fb == FB_BLANK_POWERDOWN) {
-					state_resume();
-					prev_fb = FB_BLANK_UNBLANK;
-				}
-				break;
-			case FB_BLANK_POWERDOWN:
-				if (prev_fb == FB_BLANK_UNBLANK) {
-					state_suspend();
-					prev_fb = FB_BLANK_POWERDOWN;
-				}
-				break;
-		}
-	}
-	return NOTIFY_OK;
-}
-
-
 static int __init state_notifier_init(void)
 {
-	int ret;
-
-	notif.notifier_call = fb_notifier_callback;
-	ret = fb_register_client(&notif);
-	if (ret)
-		pr_err("Failed to register FB notifier callback for state notifier.\n");
-
-	susp_wq =
-	    alloc_workqueue("state_susp_wq", WQ_FREEZABLE, 0);
+	susp_wq = create_singlethread_workqueue("state_susp_wq");
 	if (!susp_wq)
 		pr_err("State Notifier failed to allocate suspend workqueue\n");
 
 	INIT_DELAYED_WORK(&suspend_work, _suspend_work);
 	INIT_WORK(&resume_work, _resume_work);
 
-	return ret;
+	return 0;
 }
 
-late_initcall(state_notifier_init);
+subsys_initcall(state_notifier_init);
 
 MODULE_AUTHOR("Pranav Vashi <neobuddy89@gmail.com>");
 MODULE_DESCRIPTION("State Notifier Driver");
