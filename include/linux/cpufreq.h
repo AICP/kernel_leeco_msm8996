8b602ca50efb861079c933c409b4750d04a56c33
engstk <eng.stk@sapo.pt>
2016-12-13 01:10:43 +0100

    blue active
    Change-Id: I17d7424ec67a0c4ec62bc8febb575cb3e7859e2d

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -525,6 +525,9 @@ extern struct cpufreq_governor cpufreq_gov_lionfish;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #endif
 
 /*********************************************************************
