4615b0e0e983ce3e4df305bbe61b4426dd47a58f
Slawek <yarpiin@gmail.com>
2017-02-27 19:55:39 +0000

    cpufreq: Gov: Introduce cpufreq_nebula: Based on Interactive ported from msm-4.4
    Interactive based gov ported from linux 4.4 used in Nebula Kernel created by @Eliminater74
    
    Change-Id: Ibdbf756489e8831b11fd347b3f8a28921712b380

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -507,6 +507,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEBULA)
+extern struct cpufreq_governor cpufreq_gov_nebula;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nebula)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
 extern struct cpufreq_governor cpufreq_gov_lionfish;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_lionfish)
