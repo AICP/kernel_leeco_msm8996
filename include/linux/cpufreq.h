1147793d5a5086f690ad26cd6ecdde619a9c2afd
DespairFactor <matthewalex@linux.com>
2017-04-20 22:31:46 +0100

    Add despair governor
    Change-Id: I7da109eecfa089be06c4fae897426250935a07ff

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -561,6 +561,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DESPAIR)
+extern struct cpufreq_governor cpufreq_gov_despair;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_despair)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
