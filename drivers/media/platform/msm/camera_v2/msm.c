cbdf3c25c1b8d8dba4fe3aaba1058ddce1231d58
Ícaro Hoff <icarohoff@gmail.com>
2017-05-25 10:51:11 -0300

    camera_v2: port oneplus OEM changes to our LA.UM Kernel
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -83,6 +83,7 @@ spinlock_t msm_pid_lock;
 			__q->len--;				\
 			list_del_init(&node->member);		\
 			kzfree(node);				\
+			node = NULL;             \
 			break;					\
 		}						\
 	}							\
@@ -100,6 +101,7 @@ spinlock_t msm_pid_lock;
 			__q->len--;				\
 			list_del_init(&node->member);		\
 			kzfree(node);				\
+			node = NULL;   \
 			break;					\
 		}						\
 	}							\
@@ -119,6 +121,7 @@ spinlock_t msm_pid_lock;
 			if (&node->member) \
 				list_del_init(&node->member);		\
 			kzfree(node);	\
+			node = NULL;    \
 		}	\
 	}	\
 	spin_unlock_irqrestore(&__q->lock, flags);		\
@@ -148,7 +151,7 @@ typedef int (*msm_queue_find_func)(void *d1, void *d2);
 	typeof(node) __ret = NULL; \
 	msm_queue_find_func __f = (func); \
 	spin_lock_irqsave(&__q->lock, flags);			\
-	if (!list_empty(&__q->list)) { \
+	if (!list_empty(&__q->list) && (__q->len != 0)) { \
 		list_for_each_entry(node, &__q->list, member) \
 		if ((__f) && __f(node, data)) { \
 			__ret = node; \
