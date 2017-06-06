8fc8896b91104c9f2210b3c0c2cb69f832004d72
Ícaro Hoff <icarohoff@gmail.com>
2017-06-06 10:14:03 -0300

    camera_v2: adjust LA.UM camera resources to LA.HB compatibility
    Change-Id: Icfa8ecb9a55ee9b78897fa721e03486b1743b6f5
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -1040,8 +1040,10 @@ static int msm_open(struct file *filep)
 	BUG_ON(!pvdev);
 
 	/* !!! only ONE open is allowed !!! */
-	if (atomic_cmpxchg(&pvdev->opened, 0, 1))
+	if (atomic_read(&pvdev->opened))
 		return -EBUSY;
+
+	atomic_set(&pvdev->opened, 1);
 
 	spin_lock_irqsave(&msm_pid_lock, flags);
 	msm_pid = get_pid(task_pid(current));
