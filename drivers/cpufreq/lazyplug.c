7caf2368ac80ea52fcdbf099c74d1544382db9e8
mosimchah <mosimchah@gmail.com>
2017-07-18 01:31:50 -0400

    Some tweaks
    1. Disable gentle fair sleepers by default in ksysfs.c
    2. Disable input boost for lazyplug
    3. Disable lazyplug by default
    
    Change-Id: I318dc223509eff5a5cbf852b6e9cc0074e8412c4

--- drivers/cpufreq/lazyplug.c
+++ drivers/cpufreq/lazyplug.c
@@ -96,10 +96,10 @@ static struct work_struct lazyplug_boost;
 static struct workqueue_struct *lazyplug_wq;
 static struct workqueue_struct *lazyplug_boost_wq;
 
-static unsigned int __read_mostly lazyplug_active = 1;
+static unsigned int __read_mostly lazyplug_active = 0;
 module_param(lazyplug_active, uint, 0664);
 
-static unsigned int __read_mostly touch_boost_active = 1;
+static unsigned int __read_mostly touch_boost_active = 0;
 module_param(touch_boost_active, uint, 0664);
 
 static unsigned int __read_mostly nr_run_profile_sel = 0;
