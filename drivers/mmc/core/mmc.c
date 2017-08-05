f66530dbc5afec8d7ac7da92776f293e583844e6
celtare21 <celtare21@gmail.com>
2017-08-06 02:38:40 +0300

    modified:   arch/arm64/configs/lineage_x2_defconfig 	modified:   drivers/mmc/core/mmc.c 	modified:   fs/Kconfig 	modified:   fs/sync.c 	modified:   include/linux/fs.h 	modified:   mm/vmscan.c

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -59,40 +59,6 @@ static const unsigned int tacc_mant[] = {
 		__res & __mask;						\
 	})
 
-static const struct mmc_fixup mmc_fixups[] = {
-	/*
-	 * Certain Hynix eMMC 4.41 cards might get broken when HPI feature
-	 * is used so disable the HPI feature for such buggy cards.
-	 */
-	MMC_FIXUP_EXT_CSD_REV(CID_NAME_ANY, CID_MANFID_HYNIX,
-			      0x014a, add_quirk, MMC_QUIRK_BROKEN_HPI, 5),
-
-	/* Disable HPI feature for Kingstone card */
-	MMC_FIXUP_EXT_CSD_REV("MMC16G", CID_MANFID_KINGSTON, CID_OEMID_ANY,
-			add_quirk, MMC_QUIRK_BROKEN_HPI, 5),
-
-	/*
-	 * Some Hynix cards exhibit data corruption over reboots if cache is
-	 * enabled. Disable cache for all versions until a class of cards that
-	 * show this behavior is identified.
-	 */
-	MMC_FIXUP("H8G2d", CID_MANFID_HYNIX, CID_OEMID_ANY, add_quirk_mmc,
-		  MMC_QUIRK_CACHE_DISABLE),
-
-	MMC_FIXUP("MMC16G", CID_MANFID_KINGSTON, CID_OEMID_ANY, add_quirk_mmc,
-		  MMC_QUIRK_CACHE_DISABLE),
-
-	END_FIXUP
-};
-
-#ifdef CONFIG_ASYNC_FSYNC
-static unsigned int perf_degr;
-int emmc_perf_degr(void)
-{
-	return perf_degr;
-}
-#endif
-
 /*
  * Given the decoded CSD structure, decode the raw CID to our CID structure.
  */
