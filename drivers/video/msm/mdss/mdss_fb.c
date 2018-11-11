691526486b76b8f44ab3b1ba1fb8b2e0d7830665
sdv22 <sdv22@live.ru>
2018-11-11 08:15:58 +0300

    mdss: Fix suspend from LP for VIDEO type panels

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1492,7 +1492,8 @@ static int mdss_fb_suspend_sub(struct msm_fb_data_type *mfd)
 		 * on, but turn off all interface clocks.
 		 */
 		if (mdss_fb_is_power_on(mfd)) {
-			ret = mdss_fb_blank_sub(BLANK_FLAG_ULP, mfd->fbi,
+            int flag = mfd->panel_info->type == MIPI_CMD_PANEL ? BLANK_FLAG_ULP : BLANK_FLAG_LP;
+			ret = mdss_fb_blank_sub(flag, mfd->fbi,
 					mfd->suspend.op_enable);
 			if (ret) {
 				pr_err("can't turn off display!\n");
